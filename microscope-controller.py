b0539a7c7f9009e83fb1a6344c4fe75f2220ec88
TakaYokosawa <tyokosawa@g.ecc.u-tokyo.ac.jp>
2020-07-30 14:24:56 +0900

    initial commit

---
+++ microscope-controller.py
@@ -0,0 +1,69 @@
+import time, serial
+import numpy as np
+
+class BX3M_CBFM:
+    def __init__(self):
+        self.log_on()
+        self.objective_list = ['x5', 'x10', 'x20', 'x50', 'x100', 'not using']
+
+    def __enter__(self):
+        return self
+    
+    def __exit__(self, ex_type, ex_value, trace):
+        self.log_off()
+
+    def send_command (self, command):
+        """ Send a text command to the microscope controller"""
+        self.ser.write((command+'\r\n').encode())
+        time.sleep(0.2)
+        message = self.ser.read(self.ser.inWaiting()).decode()
+        while not message:
+            time.sleep(0.2)
+            message = self.ser.read(self.ser.inWaiting()).decode()
+            print('waiting for reply...')
+        if message == 'x\r\n':
+            print ('Maybe no command')
+            pass
+        elif message == '1x\r\n':
+            print ('maybe no control')
+            pass
+        elif message.split(' ')[1] == 'E013F0120\r\n' or message.split(' ')[1] == 'E013F0130\r\n':
+            print('maybe error')
+        else: 
+            print (command, message)
+            return message
+
+    def log_on (self):
+        """ Connect to microscope controller and return a serial port handle"""
+        self.ser = serial.Serial(
+            port='COM7',
+            baudrate=19200,
+            parity=serial.PARITY_EVEN,
+            stopbits=serial.STOPBITS_TWO,
+            bytesize=serial.EIGHTBITS
+        )
+        self.send_command('1L 2')
+
+    def log_off (self):
+        """ turn off remote control of microscope, close the port"""
+        self.send_command('1L 0')
+        self.ser.close()
+
+    def change_objective(self, target):
+        current_index = int(self.send_command('1OB?').split(' ')[1].split('\r\n')[0])
+        target_index = self.objective_list.index(target) + 1
+        for i in reversed(range(target_index, current_index, np.sign(current_index - target_index) )):
+            self.send_command(f'1OB {i}')
+
+with BX3M_CBFM() as test:
+    test.change_objective('x5') ## choose from 'x5', 'x10', 'x20', 'x50', 'x100'
+
+# [command]? ask status X: no control
+# 1MU? 1MU X
+# 1VL? 1VL X
+# 1[E, S]N1? X
+# 1[D, E, M]IL? X
+
+# 1U? 1U B3M,RV.6,B3M-HS,B3M-HSRE 
+# 1OB? 1OB 1-6: objective position
+# 1ER? 1ER E013F0120 
